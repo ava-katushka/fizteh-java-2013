b13c9865e81e6eb0d1a42e7fb5d2ff4c2ae8b734
Anna Vyatkina <ava.katushka@gmail.com>
2013-10-05 12:51:08 +0400

    Added interface command

---
+++ src/ru/fizteh/fivt/students/vyatkina/shell/Command.java
@@ -0,0 +1,12 @@
+package ru.fizteh.fivt.students.vyatkina.shell;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ava_katushka
+ * Date: 05.10.13
+ * Time: 12:50
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Command {
+
+}
